983e326f771f7f2b234687b21cce246a03aaac59
Damien Sedgwick <damiensedgwick@icloud.com>
2021-04-09 18:36:09 +0100

    Added coming soon message

---
+++ src/components/Header.tsx
@@ -0,0 +1,51 @@
+import Link from "next/link";
+
+import {
+  TiSocialGithubCircular,
+  TiSocialLinkedinCircular,
+  TiDevicePhone,
+  TiMail,
+} from "react-icons/ti";
+
+import { Container } from "./Container";
+
+export const Header = () => {
+  return (
+    <header className="py-4 shadow-2xl">
+      <Container>
+        <div className="social-links flex flex-row items-center">
+          <a
+            className="text-2xl md:text-3xl mx-2"
+            href="https://github.com/damiensedgwick"
+            target="_blank"
+          >
+            <TiSocialGithubCircular />
+          </a>
+          <a
+            className="text-2xl md:text-3xl mx-2"
+            href="https://www.linkedin.com/in/damiensedgwick/"
+            target="_blank"
+          >
+            <TiSocialLinkedinCircular />
+          </a>
+        </div>
+        <div className="logo">
+          <Link href="/">
+            <a className="text-3xl md:text-4xl">DKS</a>
+          </Link>
+        </div>
+        <div className="contact-links flex flex-row items-center">
+          <a
+            className="text-2xl md:text-3xl mx-2"
+            href="mailto:damiensedgwick@icloud.com"
+          >
+            <TiMail />
+          </a>
+          <a className="text-2xl md:text-3xl mx-2" href="tel:+447939273379">
+            <TiDevicePhone />
+          </a>
+        </div>
+      </Container>
+    </header>
+  );
+};
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,6 @@
 import Head from "next/head";
-import Link from "next/link";
 
-import {
-  TiSocialGithubCircular,
-  TiSocialLinkedinCircular,
-  TiDevicePhone,
-  TiMail,
-} from "react-icons/ti";
-
-import { Container } from "../components/Container";
+import { Header } from "../components/Header";
 
 export default function Home() {
   return (
@@ -18,42 +10,17 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <header className="py-4 shadow-2xl">
-        <Container>
-          <div className="social-links flex flex-row items-center">
-            <a
-              className="text-2xl md:text-3xl mx-2"
-              href="https://github.com/damiensedgwick"
-              target="_blank"
-            >
-              <TiSocialGithubCircular />
-            </a>
-            <a
-              className="text-2xl md:text-3xl mx-2"
-              href="https://www.linkedin.com/in/damiensedgwick/"
-              target="_blank"
-            >
-              <TiSocialLinkedinCircular />
-            </a>
-          </div>
-          <div className="logo">
-            <Link href="/">
-              <a className="text-3xl md:text-4xl">DKS</a>
-            </Link>
-          </div>
-          <div className="contact-links flex flex-row items-center">
-            <a
-              className="text-2xl md:text-3xl mx-2"
-              href="mailto:damiensedgwick@icloud.com"
-            >
-              <TiMail />
-            </a>
-            <a className="text-2xl md:text-3xl mx-2" href="tel:+447939273379">
-              <TiDevicePhone />
-            </a>
-          </div>
-        </Container>
-      </header>
+      <Header />
+
+      <main
+        className="flex flex-col justify-center items-center"
+        style={{ height: "calc(100vh - 72px" }}
+      >
+        <h1 className="text-2xl md:text-4xl mb-4">Damien Sedgwick</h1>
+        <p>
+          <i>Coming soon...</i>
+        </p>
+      </main>
     </div>
   );
 }
