8a67c70724ddc10837341603bee2a6207b066c38
Damien Sedgwick <damiensedgwick@icloud.com>
2021-05-03 15:20:51 +0100

    Added avatar image

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,5 @@
+import Image from "next/image";
+
 import { Layout } from "../components/Layout";
 
 export default function Home() {
@@ -9,9 +11,16 @@ export default function Home() {
       <section className="masthead px-6 md:px-0">
         <div
           className="container mx-auto flex flex-row justify-center items-center"
-          style={{ height: "calc(75vh)" }}
+          style={{ height: "calc(80vh)" }}
         >
           <div className="container mx-auto text-center md:w-3/4">
+            <Image
+              src="/avatar.jpg"
+              alt="Picture of Damien Sedgwick"
+              width={175}
+              height={175}
+              className="rounded-full"
+            />
             <h1 className="text-lg md:text-2xl md:mb-2">Damien Sedgwick</h1>
             <p className="text-2xl md:text-4xl pb-2">Frontend Developer</p>
             <hr className="mb-2" />
