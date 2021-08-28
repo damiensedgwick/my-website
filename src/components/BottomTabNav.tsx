ef9c8cbd9b77e1e10249739c5378ed566cf58a8c
Damien Sedgwick <damiensedgwick@icloud.com>
2021-08-28 15:38:35 +0100

    Reduced bottom tab padding and text size.

--- src/components/BottomTabNav.tsx
+++ src/components/BottomTabNav.tsx
@@ -5,33 +5,33 @@ export const BottomTabNav = () => {
   return (
     <div className="fixed w-screen bottom-0 shadow-2xl border-t-2 border-gray-800">
       <ul className="flex flex-row justify-evenly items-center">
-        <li className="w-full text-center py-4 hover:bg-gray-100">
+        <li className="w-full text-center py-2 hover:bg-gray-100">
           <Link href="/">
             <a>
               <MdHome className="mx-auto text-2xl" />
-              <span className="text-sm">Home</span>
+              <span className="text-xs">Home</span>
             </a>
           </Link>
         </li>
-        <li className="w-full text-center py-4 hover:bg-gray-100">
+        <li className="w-full text-center py-2 hover:bg-gray-100">
           <Link href="/">
             <a>
               <MdInfo className="mx-auto text-2xl" />
-              <span className="text-sm">About</span>
+              <span className="text-xs">About</span>
             </a>
           </Link>
         </li>
-        <li className="w-full text-center py-4 hover:bg-gray-100">
+        <li className="w-full text-center py-2 hover:bg-gray-100">
           <Link href="/">
             <a>
               <MdPermContactCalendar className="mx-auto text-2xl" />
-              <span className="text-sm">Contact</span>
+              <span className="text-xs">Contact</span>
             </a>
           </Link>
         </li>
-        <button className="w-full text-center mx-auto py-4 hover:bg-gray-100">
+        <button className="w-full text-center mx-auto py-2 hover:bg-gray-100">
           <MdMenu className="mx-auto text-2xl" />
-          <span className="text-sm">More</span>
+          <span className="text-xs">More</span>
         </button>
       </ul>
       {/* Home */}
