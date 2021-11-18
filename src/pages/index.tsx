7a0a3cdcf3ea5e65f881040a9c154f29a15a350e
Damien Sedgwick <damiensedgwick@icloud.com>
2021-11-18 10:48:23 +0000

    Initiated fresh nextjs site.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,93 +1,22 @@
-import Head from "next/head";
-import type { NextPage } from "next";
-import { Icon } from "@iconify/react";
-import {
-  AiOutlineGithub,
-  AiOutlineLinkedin,
-  AiOutlineTwitter,
-} from "react-icons/ai";
-
-import styles from "../styles/home.module.css";
+import type { NextPage } from 'next'
+import Head from 'next/head'
 
 const Home: NextPage = () => {
   return (
-    <div className={styles.container}>
+    <div>
       <Head>
         <title>Damien Sedgwick | Software Engineer</title>
-        <meta
-          name="description"
-          content="Damien Sedgwick is a software engineer currently working at Yozobi."
-        />
-        <link rel="stylesheet" href="/favicon.ico" />
-        <link
-          href="https://fonts.googleapis.com/css2?family=JetBrains+Mono:wght@300;400;600&display=swap"
-          rel="stylesheet"
-        />
+        <meta name="description" content="Coming soon..." />
+        <link rel="icon" href="/favicon.ico" />
       </Head>
-      <div className={styles.masthead}>
-        <div className={styles.mastheadContent}>
-          <div className={styles.mastheadBackground}>
-            <img
-              className={styles.mastheadAvatar}
-              src="/avatar.jpg"
-              alt="Picture of Damien Sedgwick"
-            />
-          </div>
-        </div>
-        <div className={styles.intro}>
-          <h1 className={styles.introTitle}>Damien Sedgwick</h1>
-          <p className={styles.introText}>
-            A software engineer with strong skills in JavaScript/TypeScript,
-            React, XState, Hasura and Graphql. I am also passionate about
-            learning new technologies and contributing to open source software
-            in my spare time.
-          </p>
-        </div>
-        {/* Add in Work cards / project cards? */}
-        <div className={styles.projectsSection}></div>
-        <div className={styles.contactSection}>
-          <ul className={styles.socialLinks}>
-            <li className={styles.socialLinkItem}>
-              <a
-                className={styles.socialLink}
-                href="https://github.com/damiensedgwick/"
-                target="_blank"
-              >
-                <AiOutlineGithub />
-              </a>
-            </li>
-            <li className={styles.socialLinkItem}>
-              <a
-                className={styles.socialLink}
-                href="https://www.linkedin.com/in/damiensedgwick/"
-                target="_blank"
-              >
-                <AiOutlineLinkedin />
-              </a>
-            </li>
-            <li className={styles.socialLinkItem}>
-              <a
-                className={styles.socialLink}
-                href="https://twitter.com/damiensedgwick"
-                target="_blank"
-              >
-                <AiOutlineTwitter />
-              </a>
-            </li>
-            <li className={styles.socialLinkItem}>
-              <a
-                className={styles.socialLink}
-                href="https://www.polywork.com/dks"
-                target="_blank"
-              >
-                <Icon icon="simple-icons:polywork" />
-              </a>
-            </li>
-          </ul>
-        </div>
-      </div>
-    </div>
-  );
-};
 
-export default Home;
+     <main>
+       <p>We are just having a make over, back soon!</p>
+     </main>
+
+
+    </div>
+  )
+}
+
+export default Home
