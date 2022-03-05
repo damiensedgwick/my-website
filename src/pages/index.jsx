837ee6d3ce67d9084ee68d966549a527abfb71f7
damiensedgwick <damiensedgwick@icloud.com>
2022-03-05 21:21:17 +0000

    Styling text

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -6,17 +6,16 @@ import { IoLogoTwitter, IoLogoGithub, IoLogoLinkedin } from 'react-icons/io5';
 const Homepage = styled('div', {
   width: '100vw',
   height: '100vh',
-  background: theme.colors.lightBackground,
-});
-
-const Grid = styled('div', {
-  width: '100vw',
-  height: '100vh',
   display: 'grid',
   gridTemplateRows: 'repeat(6, 1fr)',
   gridTemplateColumns: '1fr',
   gridGap: '0.2rem',
-  overflowX: 'hidden',
+  background: theme.colors.lightBackground,
+
+  '@lg': {
+    gridTemplateRows: 'repeat(6, 1fr)',
+    gridTemplateColumns: 'repeat(6, 1fr)',
+  },
 });
 
 const ContentWrapper = styled('div', {
@@ -26,30 +25,47 @@ const ContentWrapper = styled('div', {
   flexDirection: 'column',
   justifyContent: 'center',
   alignItems: 'center',
-  textAlign: 'center',
+
+  '@lg': {
+    gridRow: '3 / span 2',
+    gridColumn: '2 / span 3',
+    alignItems: 'start',
+  },
 });
 
 const Name = styled('p', {
   fontSize: '1.2rem',
   textTransform: 'uppercase',
+
+  '@lg': {
+    fontSize: '1.8rem',
+  },
 });
 
 const Role = styled('h1', {
   fontSize: '1.8rem',
-  margin: '0.2rem 0',
+  margin: '0.3rem 0',
   color: theme.colors.lightText,
   '-webkit-text-stroke-width': '1.25px',
   '-webkit-text-stroke-color': theme.colors.darkText,
   letterSpacing: '0.05rem',
   filter: theme.shadows.dropShadow,
+
+  '@lg': {
+    fontSize: '2.8rem',
+  },
 });
 
 const SocialLinks = styled('ul', {
   display: 'flex',
 
   li: {
-    margin: '0.35rem',
     listStyle: 'none',
+    margin: '0 0.5rem 0 0.5rem',
+
+    '@lg': {
+      margin: '0 0.75rem 0 0',
+    },
 
     a: {
       fontSize: '1.5rem',
@@ -66,54 +82,27 @@ export default function Home() {
         <title>Use Stitches with Next.js</title>
       </Head>
 
-      <Grid>
-        <ContentWrapper>
-          <Name>Damien Sedgwick</Name>
-          <Role>Frontend Developer</Role>
-          <SocialLinks>
-            <li>
-              <a href='https://linkedin.com/in/damiensedgwick/'>
-                <IoLogoLinkedin />
-              </a>
-            </li>
-            <li>
-              <a href='https://github.com/damiensedgwick/'>
-                <IoLogoGithub />
-              </a>
-            </li>
-            <li>
-              <a href='https://twitter.com/damiensedgwick'>
-                <IoLogoTwitter />
-              </a>
-            </li>
-          </SocialLinks>
-        </ContentWrapper>
-
-        {/*<NavigationWrapper>*/}
-        {/*  <ul>*/}
-        {/*    <li>*/}
-        {/*      <Link href='#'>*/}
-        {/*        <a>About</a>*/}
-        {/*      </Link>*/}
-        {/*    </li>*/}
-        {/*    <li>*/}
-        {/*      <Link href='#'>*/}
-        {/*        <a>Work</a>*/}
-        {/*      </Link>*/}
-        {/*    </li>*/}
-        {/*    <li>*/}
-        {/*      <Link href='#'>*/}
-        {/*        <a>OSS</a>*/}
-        {/*      </Link>*/}
-        {/*    </li>*/}
-        {/*    <li>*/}
-        {/*      <Link href='#'>*/}
-        {/*        <a>Blog</a>*/}
-        {/*      </Link>*/}
-        {/*    </li>*/}
-        {/*  </ul>*/}
-        {/*</NavigationWrapper>*/}
-      </Grid>
+      <ContentWrapper>
+        <Name>Damien Sedgwick</Name>
+        <Role>Frontend Developer</Role>
+        <SocialLinks>
+          <li>
+            <a href='https://linkedin.com/in/damiensedgwick/'>
+              <IoLogoLinkedin />
+            </a>
+          </li>
+          <li>
+            <a href='https://github.com/damiensedgwick/'>
+              <IoLogoGithub />
+            </a>
+          </li>
+          <li>
+            <a href='https://twitter.com/damiensedgwick'>
+              <IoLogoTwitter />
+            </a>
+          </li>
+        </SocialLinks>
+      </ContentWrapper>
     </Homepage>
   );
 }
