62e137519598730cbde2b022bf07e68e1ffc4dbe
damiensedgwick <damiensedgwick@icloud.com>
2022-03-07 19:25:33 +0000

    Amended theme toggle positioning

--- src/components/ThemeToggleButton.tsx
+++ src/components/ThemeToggleButton.tsx
@@ -3,26 +3,27 @@ import { styled } from '../../stitches.config';
 export const ThemeToggleButton = styled('button', {
   fontSize: '1rem',
   gridRow: '1 / span 1',
-  gridColumn: '3 / span 1',
+  gridColumn: '2 / span 1',
   border: 'none',
   background: 'none',
   maxWidth: '40px',
   maxHeight: '40px',
-  margin: '0 0 0 auto',
+  margin: '0 auto',
 
   '@sm': {
-    gridColumn: '5 / span 1',
+    gridColumn: '3 / span 1',
   },
 
   '@md': {
-    gridColumn: '7 / span 1',
+    gridColumn: '4 / span 1',
   },
 
   '@lg': {
-    gridColumn: '9 / span 1',
+    gridColumn: '8 / span 1',
+    margin: '0 0 0 auto',
   },
 
   '@xl': {
-    gridColumn: '11 / span 1',
+    gridColumn: '9 / span 1',
   },
 });
