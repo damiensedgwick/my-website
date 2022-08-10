1b49da7637c5fd3a6cf370d89bb29c12876f1480
Damien Sedgwick <damiensedgwick@icloud.com>
2022-08-10 17:57:24 +0100

    Added meta description

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,10 +1,16 @@
-import { Header } from "~/components/header";
-import { LinksFunction } from "@remix-run/node";
-import stylesUrl from "~/styles/index.css";
+import { LinksFunction, MetaFunction } from '@remix-run/node';
+import { Header } from '~/components/header';
+import stylesUrl from '~/styles/index.css';
 
 export let links: LinksFunction = () => {
-  return [{ rel: "stylesheet", href: stylesUrl }];
+  return [{ rel: 'stylesheet', href: stylesUrl }];
 };
+
+export const meta: MetaFunction = () => ({
+  title: 'Damien Sedgwick | Software Developer',
+  description:
+    'One day, I will have written, something really meaningful, and it will go here.',
+});
 
 export default function Index() {
   return (
