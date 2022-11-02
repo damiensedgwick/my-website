e2effe00e8eef0066d5bc56b3485fd0150bd83e9
Damien Sedgwick <damienksedgwick@gmail.com>
2022-11-02 12:40:25 +0000

    tidied posts code

--- app/components/posts.tsx
+++ app/components/posts.tsx
@@ -9,28 +9,22 @@ export function Posts() {
       <h2>Recent Posts</h2>
       <div>
         <ul>
-          {articles.map((article: Article) => {
-            console.log(article.url);
-
-            // TODO: Links are not working correctly, remove prefix
-
-            return (
-              <li key={article.title}>
-                <a
-                  href={article.url}
+          {articles.map((article: Article) => (
+            <li key={article.title}>
+              <a
+                href={article.url}
+                title={article.title}
+                target='_blank'
+                rel='noreferrer'
+              >
+                <img
+                  src={`/social_images/${article.social_image}`}
+                  alt={article.title}
                   title={article.title}
-                  target='_blank'
-                  rel='noreferrer'
-                >
-                  <img
-                    src={`/social_images/${article.social_image}`}
-                    alt={article.title}
-                    title={article.title}
-                  />
-                </a>
-              </li>
-            );
-          })}
+                />
+              </a>
+            </li>
+          ))}
         </ul>
       </div>
     </div>
