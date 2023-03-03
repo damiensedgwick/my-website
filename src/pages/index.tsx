ef61ee47b4dd9d108afab74d21d23321fd638cbf
Damien Sedgwick <damienksedgwick@gmail.com>
2023-03-03 17:59:07 +0000

    working on layout

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,19 @@
 import Head from 'next/head';
+import { Container, Grid, Text, Card } from '@nextui-org/react';
 
 export default function Home() {
+  const MockItem = ({ text }: { text: string }) => {
+    return (
+      <Card css={{ h: '$24', $$cardColor: '$colors$primary' }}>
+        <Card.Body>
+          <Text h6 size={15} color='white' css={{ mt: 0 }}>
+            {text}
+          </Text>
+        </Card.Body>
+      </Card>
+    );
+  };
+
   return (
     <>
       <Head>
@@ -12,7 +25,34 @@ export default function Home() {
         <meta name='viewport' content='width=device-width, initial-scale=1' />
         <link rel='icon' href='/favicon.ico' />
       </Head>
-      <main></main>
+      <main>
+        <Grid.Container gap={2} justify='center' alignItems='center'>
+          <Grid xs={12} sm={5} direction='column' alignItems='center'>
+            <Text
+              h1
+              size={45}
+              css={{
+                textGradient: '45deg, $blue600 -20%, $pink600 50%',
+              }}
+              weight='bold'
+            >
+              Damien Sedgwick
+            </Text>
+            <Text
+              size={45}
+              css={{
+                textGradient: '45deg, $purple600 -20%, $pink600 100%',
+              }}
+              weight='bold'
+            >
+              Fullstack Engineer
+            </Text>
+          </Grid>
+          <Grid xs={12} sm={5}>
+            <MockItem text='2 of 2' />
+          </Grid>
+        </Grid.Container>
+      </main>
     </>
   );
 }
