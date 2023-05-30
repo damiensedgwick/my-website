0bbc054e5e719a3b8f2ee8ad9b02d81a68bd869e
Damien Sedgwick <damienksedgwick@gmail.com>
2023-05-30 14:03:02 +0100

    created new next project and wiped current build

--- next.config.js
+++ next.config.js
@@ -1,11 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  experimental: {
-    appDir: true,
-  },
-  images: {
-    domains: ['dev.to'],
-  },
-};
+const nextConfig = {}
 
-module.exports = nextConfig;
+module.exports = nextConfig
