65afec059dcfb3eec85e681528f7dd3ee312faa1
Damien Sedgwick <damienksedgwick@gmail.com>
2024-01-09 22:50:19 +0000

    added modal bg style

--- src/lib/components/ModalContent.svelte
+++ src/lib/components/ModalContent.svelte
@@ -4,7 +4,7 @@
   const modalStore = getModalStore();
 </script>
 
-<div class="p-8 rounded-md space-y-4 max-w-[89ch]">
+<div class="p-8 rounded-md space-y-4 max-w-[89ch] variant-glass-surface">
   {#if $modalStore[0]}
     <header>
       <h2 class="h2 text-center">{$modalStore[0].title}</h2>
@@ -12,39 +12,39 @@
     <article>
       <p>{$modalStore[0].body}</p>
     </article>
-    <div class="logo-cloud grid-cols-1 lg:!grid-cols-4 gap-1">
-      <a
-        class="logo-item bg-gradient-to-br variant-gradient-secondary-tertiary"
-        href="https://cli.github.com/"
-        target="_blank"
-        rel="norefer noopener"
-      >
-        <span>GitHub CLI</span>
-      </a>
-      <a
-        class="logo-item bg-gradient-to-br variant-gradient-secondary-tertiary"
-        href="https://remix.run/"
-        target="_blank"
-        rel="norefer noopener"
-      >
-        <span>Remix</span>
-      </a>
-      <a
-        class="logo-item bg-gradient-to-br variant-gradient-secondary-tertiary"
-        href="https://xstate-catalogue.com/"
-        target="_blank"
-        rel="norefer noopener"
-      >
-        <span>XState Catalogue</span>
-      </a>
-      <a
-        class="logo-item bg-gradient-to-br variant-gradient-secondary-tertiary"
-        href="https://norfolkdevelopers.com/"
-        target="_blank"
-        rel="norefer noopener"
-      >
-        <span>Norfolk Developers</span>
-      </a>
-    </div>
   {/if}
+  <div class="logo-cloud grid-cols-1 lg:!grid-cols-4 gap-1">
+    <a
+      class="logo-item bg-gradient-to-br variant-gradient-secondary-tertiary"
+      href="https://cli.github.com/"
+      target="_blank"
+      rel="norefer noopener"
+    >
+      <span>GitHub CLI</span>
+    </a>
+    <a
+      class="logo-item bg-gradient-to-br variant-gradient-secondary-tertiary"
+      href="https://remix.run/"
+      target="_blank"
+      rel="norefer noopener"
+    >
+      <span>Remix</span>
+    </a>
+    <a
+      class="logo-item bg-gradient-to-br variant-gradient-secondary-tertiary"
+      href="https://xstate-catalogue.com/"
+      target="_blank"
+      rel="norefer noopener"
+    >
+      <span>XState Catalogue</span>
+    </a>
+    <a
+      class="logo-item bg-gradient-to-br variant-gradient-secondary-tertiary"
+      href="https://norfolkdevelopers.com/"
+      target="_blank"
+      rel="norefer noopener"
+    >
+      <span>Norfolk Developers</span>
+    </a>
+  </div>
 </div>
